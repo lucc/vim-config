faeacfb42b5b4342e3bf172fd240f9e50ae0ffb5
Lucas Hoffmann <lucc@posteo.de>
2023-04-04 20:23:41 +0200

    Remove arch linux compat from rtp

--- init.lua
+++ init.lua
@@ -3,7 +3,3 @@
 require "options"
 require "plugins"
 require "maps"
-
--- Arch linux compatibility: makes it possible to load plugins installed via
--- pacman
-vim.opt.runtimepath:append{"/usr/share/vim/vimfiles"}
